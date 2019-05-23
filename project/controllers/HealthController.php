a2f2fc4287f8c67df888766dd4b195fdedfaaf29
DELL <179611207@qq.com>
2019-05-23 11:26:56 +0800

    test

--- project/controllers/HealthController.php
+++ project/controllers/HealthController.php
@@ -421,7 +421,7 @@ class HealthController extends Controller {
         if(strtoupper(substr(PHP_OS,0,3))==='WIN'){
             $command="call ".Yii::getAlias('@webroot') ."/data/git.sh {$id}";
         }else{
-            $command=Yii::getAlias('@webroot') ."/data/git.sh {$targetPath}";
+            $command=Yii::getAlias('@webroot') ."/data/git.sh {$targetPath} ".time();
         } 
         exec($command.' 2>&1',$output,$status);
         var_dump($output);
