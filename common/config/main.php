40821d2b47f1329950638d0351a883bc30752bcd
dell <admin@ds9b.com>
2019-06-03 15:15:19 +0800

    定时执行 提交任务

--- common/config/main.php
+++ common/config/main.php
@@ -4,12 +4,13 @@ return [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'timeZone' => 'Asia/Shanghai',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
-        'timeZone' => 'Asia/Shanghai',
+        
         'authManager' => [
             'class' => 'yii\rbac\DbManager',
         ],
