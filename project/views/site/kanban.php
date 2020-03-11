f225273d43430e5bdc50e34dd01bc788bc6451dd
dell <admin@ds9b.com>
2020-03-11 13:35:00 +0800

    资源引用路径

--- project/views/site/kanban.php
+++ project/views/site/kanban.php
@@ -89,11 +89,11 @@ $this->title = '看板';
     <div class="back"></div>
 </div>
 <?php
-$this->registerCssFile(CommonHelper::getImage('/css/kanban.css'), ['depends' => ['project\assets\KanBanAsset']]);
+$this->registerCssFile('/css/kanban.css', ['depends' => ['project\assets\KanBanAsset']]);
 $this->registerJsFile(CommonHelper::getImage('/js/jquery.js'), ['depends' => ['project\assets\KanBanAsset']]);
 $this->registerJsFile(CommonHelper::getImage('/js/echarts.min.js'), ['depends' => ['project\assets\KanBanAsset']]);
 $this->registerJsFile(CommonHelper::getImage('/js/china.js'), ['depends' => ['project\assets\KanBanAsset']]);
-$this->registerJsFile(CommonHelper::getImage('/js/kanban.js'), ['depends' => ['project\assets\KanBanAsset']]);
+$this->registerJsFile('/js/kanban.js', ['depends' => ['project\assets\KanBanAsset']]);
 ?>
 <script>
     <?php $this->beginBlock('dataview') ?>
