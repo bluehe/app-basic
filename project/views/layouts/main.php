16ec75d61e282de019a5cf8b9b3346232032d2a1
dell <admin@ds9b.com>
2020-04-26 10:10:52 +0800

    test

--- project/views/layouts/main.php
+++ project/views/layouts/main.php
@@ -5,23 +5,24 @@
 use yii\helpers\Html;
 
 project\assets\AppAsset::register($this);
-   
 
 dmstr\web\AdminLteAsset::register($this);
 
 $directoryAsset = Yii::$app->assetManager->getPublishedUrl('project/web');
 ?>
-    <?php $this->beginPage() ?>
-    <!DOCTYPE html>
-    <html lang="<?= Yii::$app->language ?>">
-    <head>
-        <meta charset="<?= Yii::$app->charset ?>"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <?= Html::csrfMetaTags() ?>
-        <title><?= Html::encode($this->title) ?></title>
-        <?php $this->head() ?>
-    </head>
-    <body class="hold-transition skin-blue sidebar-mini">
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+
+<head>
+    <meta charset="<?= Yii::$app->charset ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+
+<body class="hold-transition skin-blue sidebar-mini">
     <?php $this->beginBody() ?>
     <div class="wrapper">
 
@@ -44,6 +45,7 @@ $directoryAsset = Yii::$app->assetManager->getPublishedUrl('project/web');
     </div>
 
     <?php $this->endBody() ?>
-    </body>
-    </html>
-    <?php $this->endPage() ?>
+</body>
+
+</html>
+<?php $this->endPage() ?>
